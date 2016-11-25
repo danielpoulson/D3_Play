366fb7e24829ce9b7747d5c6cbdf6df48580ecef
danielpoulson <danielpoulson@hotmail.com.au>
2016-11-26 06:34:55 +1100

    --d--

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var Chart_Bar = require('./Chart-Bar3');
+var Chart_Bar = require('./Chart-Bar');
 var Chart_Bar2 = require('./Chart-Bar4');
 var Chart_Pie1 = require('./Chart-Pie');
 
@@ -17,11 +17,8 @@ var APP =
 			<div className="container">
 			<h1>This is my chart examples</h1>
 			  <div className="row">
-				<div className="one-half column"><Chart_Bar /></div>
 				<div className="one-half column"><Chart_Pie1 /></div>
-			 </div>
-			 <div className="row">
-				<div className="one-half column"><Chart_Bar2 /></div>
+				<div className="one-half column"><Chart_Bar /></div>
 			 </div>
 			</div>
 			)
